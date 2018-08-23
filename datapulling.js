92f6b609ff8387ddf9d6dab9d8e52e9e7d5e8652
vinbat <vincentbatteast@gmail.com>
2018-08-22 23:22:53 -0400

    pulling all compubox links

--- datapulling.js
+++ datapulling.js
@@ -1,14 +1,17 @@
 var https = require('https');
-
+var data = '';
 const req = https.get("https://www.boxingscene.com", (res) => {
   console.log(`STATUS: ${res.statusCode}`);
   console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
   res.setEncoding('utf8');
   res.on('data', (chunk) => {
-    console.log(`BODY: ${chunk}`);
+    data += chunk;
   });
   res.on('end', () => {
     console.log('No more data in response.');
+      var lookingfor = /(<a.*>.*compubox.*<\/a>)/gi;
+  var found = data.match(lookingfor);
+  console.log(found);
   });
 });
 req.on('error', (e) => {
